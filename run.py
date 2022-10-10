4008dbe85af8a90c934f81129b24639a570058b2
ali on GPU <dadsetan.ali@gmail.com>
2022-10-10 09:31:57 +0000

    adding color working, seperating transformations

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from torchvision import datasets
 
 from dataset.visda_dataset import VisdaDataset, VisdaTrainDataset, VisdaValidDataset
-from transformations import contrast_transforms
+from transformations import contrast_valid_transforms, contrast_train_transforms
 from util import create_samples
 
 import argparse
@@ -16,9 +15,9 @@ args = parser.parse_args()
 
 if args.action == "finetune":
     storage_path = Path(args.storage)
-    train_dataset = VisdaTrainDataset((storage_path/'train').resolve(), transform=contrast_transforms,n_views=5)
+    train_dataset = VisdaTrainDataset((storage_path/'train').resolve(), transform=contrast_train_transforms,n_views=5)
 
-    valid_dataset = VisdaValidDataset((storage_path/'validation').resolve(), transform=contrast_transforms)
+    valid_dataset = VisdaValidDataset((storage_path/'validation').resolve(), transform=contrast_valid_transforms)
     valid_dataset.set_param(5)
 
     unsupervised_dataset = VisdaDataset(train_dataset,valid_dataset)
--- transformations.py
+++ transformations.py
@@ -1,15 +1,45 @@
 from torchvision import transforms
+import random
+import math
+from PIL.ImageOps import colorize, grayscale
 
-contrast_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+def random_255():
+    return math.floor(random.random() * 255)
+
+class RandomColorize(object):
+    def __init__(self, p=.1):
+        self.p = p
+    
+    def __call__(self, sample):
+        if random.random() < self.p:
+            random_color = [random_255() for _ in range(3)]
+            return colorize(grayscale(sample),black=random_color,white=(255,255,255))
+        return sample
+
+contrast_valid_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
                                           transforms.RandomResizedCrop(size=96,scale=(.25,.5)),
                                           transforms.RandomApply([
-                                              transforms.ColorJitter(brightness=0.5, 
-                                                                     contrast=0.5, 
+                                              transforms.ColorJitter(brightness=0.8, 
+                                                                     contrast=0.8, 
                                                                      saturation=0.5, 
-                                                                     hue=0.3)
+                                                                     hue=0.2)
                                           ], p=0.8),
                                           transforms.RandomGrayscale(p=0.2),
                                           transforms.GaussianBlur(kernel_size=9),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize((0.5,), (0.5,))
+                                         ])
+
+contrast_train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                          transforms.RandomResizedCrop(size=96,scale=(.25,.5)),
+                                          RandomColorize(.8),
+                                          transforms.RandomApply([
+                                              transforms.ColorJitter(brightness=0.8, 
+                                                                     contrast=0.8, 
+                                                                     saturation=0.5, 
+                                                                     hue=0.2)
+                                          ], p=0.8),
+                                          transforms.GaussianBlur(kernel_size=3),
                                           transforms.ToTensor(),
                                           transforms.Normalize((0.5,), (0.5,))
                                          ])

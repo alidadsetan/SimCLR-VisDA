2a37ed8c354c43e1431ce2333c8df7cf0bd924eb
ali <dadsetan.ali@gmail.com>
2022-10-28 17:23:49 +0330

    sss

--- run.py
+++ run.py
@@ -107,7 +107,7 @@ if args.action == "pretrain":
 
     tensor_logger_path = Path(args.log_directory)/'tensorboard'
     wandb_logger_path = Path(args.log_directory)/'wandb'
-    trainer = pl.Trainer(callbacks=callbacks, accelerator="dp", gpus=args.num_gpus, logger=[TensorBoardLogger(
+    trainer = pl.Trainer(callbacks=callbacks, accelerator="cuda", gpus=args.num_gpus, logger=[TensorBoardLogger(
         save_dir=tensor_logger_path)]#, WandbLogger(save_dir=wandb_logger_path, project="SimCLR-VisDA")]
         , max_epochs=args.pretrain_epochs)
     trainer.fit(model, train_dataloaders=train_dataloader)

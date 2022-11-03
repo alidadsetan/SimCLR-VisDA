c38d8669019cf9cbf3efc5525f7398b8c52e47ba
ali <dadsetan.ali@gmail.com>
2022-11-03 23:20:51 +0330

    horovod

--- util.py
+++ util.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 def create_samples(data,save_directory, num_samples=6):
     random_indexes = random.choices(range(len(data)),k=num_samples)
     imgs = torch.stack([img for idx in random_indexes for img in data[idx][0]], dim=0)
-    # print([train_data[idx][1] for idx in random_indexes])
     img_grid = torchvision.utils.make_grid(imgs, nrow=6, normalize=True, pad_value=0.9)
     img_grid = img_grid.permute(1, 2, 0)
 
